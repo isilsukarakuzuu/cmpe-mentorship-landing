da4285533258b9fa3636e53158fa873ffbf30f54
Isil Su Karakuzu <isilsukarakuzu1967@gmail.com>
2025-04-28 18:20:46 +0300

    Updates

---
+++ assets/Mentors-Szb1A_Lq.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-Bq_4Tx1m.js";function t(){return e.jsx("div",{className:"p-6",children:e.jsx("h1",{className:"text-3xl font-bold",children:"Mentors Page"})})}export{t as default};
