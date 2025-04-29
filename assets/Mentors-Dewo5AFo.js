7989b46e2c0d59ab9b8215799f98f356a4eecf73
Isil Su Karakuzu <isilsukarakuzu1967@gmail.com>
2025-04-29 16:31:42 +0300

    Updates

--- assets/Mentors-Dewo5AFo.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-CtuxgIU5.js";function t(){return e.jsx("div",{className:"p-6",children:e.jsx("h1",{className:"text-3xl font-bold",children:"Mentors Page"})})}export{t as default};
